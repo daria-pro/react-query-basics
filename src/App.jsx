6015e6c2544aeff7103a28020e4ea8735038ae4f
daria-pro <shaulkod@gmail.com>
2023-08-19 14:30:14 +0300

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+
+function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+    // queryFn: () => Promise.reject("Error Message"),
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)} </pre>;
+  return <h1>TanStack Query</h1>;
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default App;
